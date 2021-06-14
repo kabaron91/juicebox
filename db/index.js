0f8a963751137f0af2cd9cbdf7ccb3fbf032a60f
Kate <kabaron91@gmail.com>
2021-06-13 19:52:47 -0500

    Heroku ready

--- db/index.js
+++ db/index.js
@@ -2,7 +2,12 @@
 
 const { Client } = require('pg')
 
-const client = new Client('postgres://localhost:5432/juicebox-dev')
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev'
+let config = { connectionString }
+if (process.env.DATABASE_URL) {
+    config.ssl = { rejectUnauthorized: false }
+}
+const client = new Client(config)
 
 // USER Methods
 
